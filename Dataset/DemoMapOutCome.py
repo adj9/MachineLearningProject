7fe6f58b5d18e521461f24f781d642c0be1a0f39
adj9 <ale.lumaca@hotmail.it>
2015-06-08 17:48:38 +0200

    csv standard module import

--- Dataset/DemoMapOutCome.py
+++ Dataset/DemoMapOutCome.py
@@ -3,10 +3,18 @@ __author__ = 'Alessandro'
 import Dataset.MapOutCome
 from Dataset.MapOutCome import *
 
+#https://docs.python.org/2/library/csv.html#csv-fmt-params
 
+# moc = MapOutCome('Fiori.csv')
+# print('File path: ', moc.get_path_file())
+# moc.run()
+#
+# print(moc.get_map())
 
-moc = MapOutCome('Fiori.csv')
-print('File path: ', moc.get_path_file())
-moc.run()
+import csv
 
-print(moc.get_map())
+with open('Fiori.csv', 'r') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',')
+    for row in spamreader:
+        print(row)
+
